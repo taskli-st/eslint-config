f78999b93f7e633675a57e44e6b7df69ad2fedd4
Alexander Behrens <alexander.behrens@speakap.nl>
2023-03-17 21:37:59 +0100

    adding prettier

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/eslint-recommended-requiring-type-checking"
+        "plugin:@typescript-eslint/eslint-recommended-requiring-type-checking",
+        "prettier"
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
