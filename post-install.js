947af1ae2d48281df8d4185ac67ae94195b2a921
Alexander Behrens <alexander.behrens@speakap.nl>
2023-03-17 21:13:25 +0100

    adding gitignore

--- post-install.js
+++ post-install.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 if (!fs.existsSync(path.resolve(process.env.INIT_CWD, "./.eslintrc.js"))) {
-    fs.copyFileSync(path.resolve(process.env.INIT_CWD, "./node_modules/@taskli-st/tsconfig/.eslintrc.js"), path.resolve(process.env.INIT_CWD, "./.eslintrc.js"));
+    fs.copyFileSync(path.resolve(process.env.INIT_CWD, "./node_modules/@taskli-st/eslint-config/.eslintrc.js"), path.resolve(process.env.INIT_CWD, "./.eslintrc.js"));
 }
 
 if (!fs.existsSync(path.resolve(process.env.INIT_CWD, "./.eslintignore"))) {
-    fs.copyFileSync(path.resolve(process.env.INIT_CWD, "./node_modules/@taskli-st/tsconfig/.eslintignore"), path.resolve(process.env.INIT_CWD, "./.eslintignore"));
+    fs.copyFileSync(path.resolve(process.env.INIT_CWD, "./node_modules/@taskli-st/eslint-config/.eslintignore"), path.resolve(process.env.INIT_CWD, "./.eslintignore"));
 }
