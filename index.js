d91c6868f336d347bae68c2396a7276002f1dc96
Alexander Behrens <alexander.behrens@speakap.nl>
2023-03-18 21:14:22 +0100

    fixing extends

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = {
     },
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/eslint-recommended-requiring-type-checking",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "prettier"
     ],
     parser: "@typescript-eslint/parser",
